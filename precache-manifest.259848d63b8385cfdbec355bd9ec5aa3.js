2aee7d73d588d69ac410ab65a6d2a041733dfbe9
khaalid abdullah <khaalidabdullah@khaalids-MacBook-Pro.local>
2018-12-26 13:12:23 -0800

    gh deploy

---
+++ precache-manifest.259848d63b8385cfdbec355bd9ec5aa3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ec195010ddcf8b027a33",
+    "url": "/static/js/main.ec195010.chunk.js"
+  },
+  {
+    "revision": "fbe3ae89c6fe7135ec22",
+    "url": "/static/js/1.fbe3ae89.chunk.js"
+  },
+  {
+    "revision": "ec195010ddcf8b027a33",
+    "url": "/static/css/main.300a4735.chunk.css"
+  },
+  {
+    "revision": "23fcf31602afbf18cb23e69f345d9c33",
+    "url": "/index.html"
+  }
+];
